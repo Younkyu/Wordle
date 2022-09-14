997e904f54fc7600af59d0f09597a7f1c2c11802
Younkyu <younkyu2004@gmail.com>
2022-09-14 03:39:38 -0700

    implemented main gameplay

--- src/app/components/attempt/attempt.component.ts
+++ src/app/components/attempt/attempt.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, HostListener, EventEmitter } from '@angular/core';
+import { BodyComponent } from '../body/body.component';
 
 @Component({
   selector: 'app-attempt',
@@ -10,7 +11,9 @@ export class AttemptComponent implements OnInit {
   @Input() ready: string | undefined;
 
   @Output() changeReady = new EventEmitter();
+  @Output() finish = new EventEmitter();
 
+  word: string = "";
   typed: string | undefined;
   letter0: string | undefined;
   letter1: string | undefined;
@@ -26,11 +29,41 @@ export class AttemptComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  checkWord() {
+    var divs = document.getElementById("" + this.rowNum)?.querySelectorAll(".letterBox");
+    var wordArray = this.word.split('');
+    // in case for multiple yellow marks when there should be only one yellow
+    for (let x = 0; x < 5; x++) {
+      if(wordArray.includes(BodyComponent.wordle.charAt(x))) {
+        wordArray.splice(wordArray.indexOf(BodyComponent.wordle.charAt(x)), 1);
+      }
+    }
+    // colorcode letter boxes
+    for(let i = 0; i < 5; i++) {
+      if (this.word.charAt(i) === BodyComponent.wordle.charAt(i)) {
+        divs?.item(i).classList.add('fullyCorrect');
+      } else if (BodyComponent.wordle.indexOf(this.word.charAt(i)) !== -1) {
+        if (wordArray.includes(this.word.charAt(i))) {
+          divs?.item(i).classList.add('wrong');
+        } else {
+          divs?.item(i).classList.add('partiallyCorrect');
+        }
+      } else {
+        divs?.item(i).classList.add('wrong');
+      }
+    }
+    // check for win
+    if (this.word === BodyComponent.wordle) {
+      this.finish.emit();
+    }
+  }
+
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
     if (this.ready === 'yes') {
       this.typed = event.key.toUpperCase();
       if (this.typed === 'BACKSPACE' && this.index > 0) {
+        this.word = this.word.substring(0, this.word.length - 1);
         this.index--;
 
         switch (this.index) {
@@ -59,7 +92,8 @@ export class AttemptComponent implements OnInit {
         }
 
       } else if (this.typed === "ENTER" && this.index >= 5) {
-        console.log("CONFIRMING");
+        console.log("SUBMITTED WORD: " + this.word);
+        this.checkWord();
         this.changeReady.emit();
       } else if (AttemptComponent.alphabet.includes(this.typed)) {
         if (this.index < 5) {
@@ -67,22 +101,27 @@ export class AttemptComponent implements OnInit {
           switch (this.index) {
             case 0:
               this.letter0 = this.typed;
+              this.word += this.letter0;
               break;
 
             case 1:
               this.letter1 = this.typed;
+              this.word += this.letter1;
               break;
 
             case 2:
               this.letter2 = this.typed;
+              this.word += this.letter2;
               break;
 
             case 3:
               this.letter3 = this.typed;
+              this.word += this.letter3;
               break;
 
             case 4:
               this.letter4 = this.typed;
+              this.word += this.letter4;
               break;
 
             default:
--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -14,6 +14,32 @@ export class BodyComponent implements OnInit {
   @Input() ready4: string = "no";
   @Input() ready5: string = "no";
 
+  // answer options
+  static dictionary = ['ABUSE', 'ADULT', 'AGENT', 'ANGER', 'APPLE', 'AWARD', 'BASIS', 'BEACH', 'BENCH', 'BIPED',
+                        'BIRTH', 'BLOCK', 'BLOOD', 'BOARD', 'BRAIN', 'BREAD', 'BREAK', 'BROWN', 'BUYER', 'CAUSE',
+                        'CHAIN', 'CHAIR', 'CHEST', 'CHIEF', 'CHILD', 'CHINA', 'CLAIM', 'CLASS', 'CLOCK', 'COACH',
+                        'COAST', 'COURT', 'COVER', 'CREAM', 'CRIME', 'CROSS', 'CROWD', 'CROWN', 'CYCLE', 'DANCE',
+                        'DEATH', 'DEPTH', 'DOUBT', 'DRAFT', 'DRAMA', 'DREAM', 'DRESS', 'DRINK', 'DRIVE', 'EARTH',
+                        'ENEMY', 'ENTRY', 'ERROR', 'EVENT', 'FAITH', 'FAULT', 'FIELD', 'FIGHT', 'FINAL', 'FLOOR',
+                        'FOCUS', 'FORCE', 'FRAME', 'FRANK', 'FRONT', 'FRUIT', 'GLASS', 'GRANT', 'GRASS', 'GREEN',
+                        'GROUP', 'GUIDE', 'HEART', 'HENRY', 'HORSE', 'HOTEL', 'HOUSE', 'IMAGE', 'INDEX', 'INPUT',
+                        'ISSUE', 'JAPAN', 'JONES', 'JUDGE', 'KNIFE', 'LAURA', 'LAYER', 'LEVEL', 'LEWIS', 'LIGHT',
+                        'LIMIT', 'LUNCH', 'MAJOR', 'MARCH', 'MATCH', 'METAL', 'MODEL', 'MONEY', 'MONTH', 'MOTOR',
+                        'MOUTH', 'MUSIC', 'NIGHT', 'NOISE', 'NORTH', 'NOVEL', 'NURSE', 'OFFER', 'ORDER', 'OTHER',
+                        'OWNER', 'PANEL', 'PAPER', 'PARTY', 'PEACE', 'PETER', 'PHASE', 'PHONE', 'PIECE', 'PILOT',
+                        'PITCH', 'PLACE', 'PLANE', 'PLANT', 'PLATE', 'POINT', 'POUND', 'POWER', 'PRESS', 'PRICE',
+                        'PRIDE', 'PRIZE', 'PROOF', 'QUEEN', 'RADIO', 'RANGE', 'RATIO', 'REPLY', 'RIGHT', 'RIVER',
+                        'ROUND', 'ROUTE', 'RUGBY', 'SCALE', 'SCENE', 'SCOPE', 'SCORE', 'SENSE', 'SHAPE', 'SHARE',
+                        'SHEEP', 'SHEET', 'SHIFT', 'SHIRT', 'SHOCK', 'SIGHT', 'SIMON', 'SKILL', 'SLEEP', 'SMILE',
+                        'SMITH', 'SMOKE', 'SOUND', 'SOUTH', 'SPACE', 'SPEED', 'SPITE', 'SPORT', 'SQUAD', 'STAFF',
+                        'STAGE', 'START', 'STATE', 'STEAM', 'STEEL', 'STOCK', 'STONE', 'STORE', 'STUDY', 'STUFF',
+                        'STYLE', 'SUGAR', 'TABLE', 'TASTE', 'TERRY', 'THEME', 'THING', 'TITLE', 'TOTAL', 'TOUCH',
+                        'TOWER', 'TRACK', 'TRADE', 'TRAIN', 'TREND', 'TRIAL', 'TRUST', 'TRUTH', 'UNCLE', 'UNION',
+                        'UNITY', 'VALUE', 'VIDEO', 'VISIT', 'VOICE', 'WASTE', 'WATCH', 'WATER', 'WHILE', 'WHITE',
+                        'WHOLE', 'WOMAN', 'WORDS', 'WORLD', 'WORRY', 'WORST', 'YIELD', 'YOUNG', 'YOUTH', 'ZONES'];
+
+  static wordle = BodyComponent.dictionary[Math.floor(Math.random() * BodyComponent.dictionary.length)];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -57,8 +83,18 @@ export class BodyComponent implements OnInit {
   toggleReady5() {
     if (this.ready0 === "no" && this.ready1 === "no" && this.ready2 === "no" && this.ready3 === "no" && this.ready4 === "no" && this.ready5 === "yes") {
       this.ready5 = "no";
-      console.log("EOG");
+      console.log("EOG: LOSS\nANSWER: " + BodyComponent.wordle);
     }
+  }
+
+  toggleWin() {
+    this.ready0 = "no";
+    this.ready1 = "no";
+    this.ready2 = "no";
+    this.ready3 = "no";
+    this.ready4 = "no";
+    this.ready5 = "no";
+    console.log("EOG: WIN");
   }
 
 }
